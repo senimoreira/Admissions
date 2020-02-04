aba1dc82f0526a174fdaa59bc76b5758fe4119d8
senimoreira <47533831+senimoreira@users.noreply.github.com>
2020-02-04 17:26:28 +0100

    Add files via upload

---
+++ ExternalCandidatePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="external-candidate">
+    <div class="centered">
+      <h1> External Candidate </h1>
+      <p> What is your email? </p>
+      <input
+        v-model="email">
+      <p>Email inserted is: {{ email }}</p>
+    </div>
+  </div>
+</template>
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+<script>
+export default {
+  data () {
+    return {
+      email: ''
+    }
+  }
+}
+</script>
+
+<style >
+.external-candidate {
+  text-align: center;
+}
+.centered {
+  margin-top: 15%;
+}
+</style>
---
+++ index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import store from '@/store'
+import PageWithNavBarAndFooterLayout from '@/layouts/PageWithNavBarAndFooterLayout'
+import PageNotFoundPage from '@/pages/PageNotFoundPage'
+import UnauthorizedPage from '@/pages/UnauthorizedPage'
+import HomePage from '@/pages/HomePage'
+import ExternalCandidatePage from '@/pages/ExternalCandidatePage'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: '/fenixedu-admissions',
+  routes: [
+    {
+      path: '',
+      async beforeEnter (to, from, next) {
+        await store.dispatch('fetchProfile')
+        next()
+      },
+      component: PageWithNavBarAndFooterLayout,
+      children: [
+        {
+          path: '/unauthorized',
+          name: 'UnauthorizedPage',
+          component: UnauthorizedPage
+        },
+        {
+          path: '/',
+          name: 'HomePage',
+          component: HomePage
+        },
+        {
+          path: 'external',
+          name: 'ExternalCandidatePage',
+          component: ExternalCandidatePage
+        },
+        {
+          path: '*',
+          name: 'PageNotFound',
+          component: PageNotFoundPage
+        }
+      ]
+    }
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
+})
+
+export default router
